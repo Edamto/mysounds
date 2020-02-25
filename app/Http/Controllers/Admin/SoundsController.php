f09c8f098a39e5e21ab91dac0f79b6447e2da09b
Edamoto <khemkhomfy@gmail.com>
2020-02-25 11:38:20 +0000

    commit4

--- app/Http/Controllers/Admin/SoundsController.php
+++ app/Http/Controllers/Admin/SoundsController.php
@@ -28,4 +28,15 @@ class SoundsController extends Controller
         
         return redirect('admin/article/create');
     }
-}
+    
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = Article::where('title', $cond_title)->get();
+        } else {
+            $posts = Article::all();
+        }
+        return view('admin.article.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+        }
+    }
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+
+
+@section('title', 'プロフィール')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+            </div>
+        </div>
+    </div>
+@endsection
